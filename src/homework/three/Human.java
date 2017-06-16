356f8af5e8619aeb2e115795e287d4f1766c3915
Vladislav <v.k.karaasenov@gmail.com>
2017-06-16 11:39:14 +0300

    Initial commit

---
+++ src/homework/three/Human.java
@@ -0,0 +1,14 @@
+package homework.three;
+
+public class Human {
+
+	String name;
+	String lastName;
+	int age;
+	int height;
+	double weight;
+	boolean isFemale;
+	Human[] family;
+	Human[] friends;
+
+}
